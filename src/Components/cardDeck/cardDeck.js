3e42d35c1c1aaa9104c5b493b4e2105fb3edb40c
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-10-04 23:04:48 +0530

    fix - fixed card picking bug

--- src/Components/cardDeck/cardDeck.js
+++ src/Components/cardDeck/cardDeck.js
@@ -1,36 +1,65 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./cardDeck.css";
 
 const CardDeck = () => {
-  const [deck, setDeck] = useState([]);
+  const data = [
+    "2H",
+    "3H",
+    "4H",
+    "5H",
+    "6H",
+    "7H",
+    "8H",
+    "9H",
+    "10H",
+    "AH",
+    "QH",
+    "KH",
+    "JH",
+    "2C",
+    "3C",
+    "4C",
+    "5C",
+    "6C",
+    "7C",
+    "8C",
+    "9C",
+    "10C",
+    "AC",
+    "QC",
+    "KC",
+    "JC",
+    "2D",
+    "3D",
+    "4D",
+    "5D",
+    "6D",
+    "7D",
+    "8D",
+    "9D",
+    "10D",
+    "AD",
+    "QD",
+    "KD",
+    "JD",
+    "2S",
+    "3S",
+    "4S",
+    "5S",
+    "6S",
+    "7S",
+    "8S",
+    "9S",
+    "10S",
+    "AS",
+    "QS",
+    "KS",
+    "JS",
+  ];
+  const [deck, setDeck] = useState(data);
   const [pickedCards, setPickedCards] = useState([]);
-  useEffect(() => {
-    const name = ["H", "C", "D", "S"];
-    const number = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "A",
-      "Q",
-      "K",
-      "J",
-    ];
-    let deck = [];
-
-    for (let i = 0; i < name.length; i++) {
-      for (let j = 0; j < number.length; j++) {
-        deck.push(number[j] + name[i]);
-      }
-    }
-    setDeck(deck);
-  }, []);
-
+  const [error, setError] = useState(false);
+  console.log(deck.length);
   function shuffleCard(cards) {
     let tempDeck = cards;
     for (let i = 0; i < tempDeck.length; i++) {
@@ -44,31 +73,36 @@ const CardDeck = () => {
 
   function pickFive(cards) {
     let fiveCards = [];
-    let leftOverCards = cards;
     for (let i = 0; i < 5; i++) {
-      fiveCards.push(
-        leftOverCards[Math.floor(Math.random() * leftOverCards.length)]
-      );
+      let element = cards[Math.floor(Math.random() * cards.length)];
+      fiveCards.push(element);
     }
-    leftOverCards = leftOverCards.filter((item) => !fiveCards.includes(item));
+    let leftOverCards = cards.filter((item) => !fiveCards.includes(item));
+    console.log(leftOverCards);
     return { fiveCards, leftOverCards };
   }
+
   function handleShuffle() {
     let changedArr = shuffleCard([...deck]);
     setDeck(changedArr);
   }
   function handlePick() {
-    let { fiveCards, leftOverCards } = pickFive([...deck]);
-    setPickedCards(fiveCards);
-    setDeck(leftOverCards);
+    if (deck.length < 5) {
+      setError(true);
+    } else {
+      setPickedCards([]);
+      let { fiveCards, leftOverCards } = pickFive([...deck]);
+      setPickedCards(fiveCards);
+      setDeck(leftOverCards);
+    }
   }
 
   return (
     <>
       <div className="cardContainer">
-        {deck.map((item) => {
+        {deck.map((item, i) => {
           return (
-            <div key={item} className="card">
+            <div key={i} className="card">
               <p>{item}</p>
             </div>
           );
@@ -80,10 +114,11 @@ const CardDeck = () => {
       <button className="shuffleBtn" onClick={handlePick}>
         Pick 5 cards
       </button>
+      {error && <p>No Cards Left to Pick</p>}
       <div className="pickedCardsContainer">
-        {pickedCards.map((item) => {
+        {pickedCards.map((item, i) => {
           return (
-            <div key={item} className="pickedCard">
+            <div key={i} className="pickedCard">
               <p>{item}</p>
             </div>
           );
